185b09350a15f215573d0efb7910da9a08cd51a2
Henrick Tambourou <h.tambourou@gmail.com>
2022-09-21 19:29:46 -0500

    Style top component

--- ui/src/components/Top.tsx
+++ ui/src/components/Top.tsx
@@ -5,8 +5,8 @@ const Top = () => {
         <div className="top">
             <div className="top-content">
                 <div className="row left">
-                    <h1>Involving the world in Artificail Intelligence</h1>
-                    <h2>Our mission is to get worldwide sentiment and foster discussions on artificial intelligence, to build a more inclusive space.</h2>
+                    <h1>Involving the world in Artificial Intelligence</h1>
+                    <p>Our mission is to get worldwide sentiment and foster discussions on artificial intelligence, to build a more inclusive space.</p>
                 </div>
                  <div className="row right">
                     <img src={img1}  alt="building block rendering" style={{width:"100%", height:"100%", objectFit:"contain"}}/>
